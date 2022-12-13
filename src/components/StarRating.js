f87777b3ee7ebcb384ffb6f01903714150c4d767
Samsal707 <samsal707@gmail.com>
2022-12-13 20:34:58 +1100

    react exercise state management - useState hook

---
+++ src/components/StarRating.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { FaStar } from "react-icons/fa";
+
+// export default function StarRating() {
+//   return [
+//     <FaStar color="red" />,
+//     <FaStar color="red" />,
+//     <FaStar color="red" />,
+//     <FaStar color="grey" />,
+//     <FaStar color="grey" />
+//   ];
+// }
+
+const Star = ({ selected = false, onSelect = f => f }) => (
+  <FaStar color={selected ? "red" : "grey"} onClick={onSelect} />
+);
+
+
+const createArray = length => [...Array(length)];
+
+export default function StarRating({ style = {}, totalStars = 5 }) {
+  const [selectedStars, setSelectedStars] = useState(0);
+  return (
+    <div style={{ padding: "5px", ...style }}>
+      {createArray(totalStars).map((n, i) => (
+        <Star
+          key={i}
+          selected={selectedStars > i}
+          onSelect={() => setSelectedStars(i + 1)}
+        />
+      ))}
+      <p>
+        {selectedStars} of {totalStars} stars
+      </p>
+    </div>
+  );
+}
