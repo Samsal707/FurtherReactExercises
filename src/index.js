3819b96a24ed09be2134d9febcf5c43105b5bc95
Samsal707 <samsal707@gmail.com>
2022-12-18 20:00:21 +1100

    creating and implementing context in react

--- src/index.js
+++ src/index.js
@@ -1,26 +1,13 @@
 import React, { createContext } from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
 import colors from "./data/color-data";
-import data from "./data/recipes.json";
-import Menu from "./components/Menu";
-import StarRating from "./components/StarRating";
+import { render } from "react-dom";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 export const ColorContext = createContext();
 
-
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-        <Menu recipes={data} />
-        <StarRating totalStars={5} />
-    <ColorContext.Provider value={{ colors }}>
-        <App />
-    </ColorContext.Provider>
-  </React.StrictMode>
+render(
+  <ColorContext.Provider value={{ colors }}>
+    <App />
+  </ColorContext.Provider>,
+  document.getElementById("root")
 );
-
-reportWebVitals();
