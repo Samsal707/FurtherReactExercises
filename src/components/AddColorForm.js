e6dc237d3ca3d278a03ce5ec1f51a0cfbf1f249f
Samsal707 <samsal707@gmail.com>
2022-12-18 19:56:26 +1100

    implementing custom hooks and form elements in react

--- src/components/AddColorForm.js
+++ src/components/AddColorForm.js
@@ -1,34 +1,29 @@
 
 import React, { useState } from "react";
+import { useInput } from "./hooks"
 
 export default function AddColorForm({ onNewColor = f => f }) {
-  const [title, setTitle] = useState("");
-  const [color, setColor] = useState("#000000");
+  const [titleProps, resetTitle] = useState("");
+  const [colorProps, resetColor] = useState("#000000");
 
 
-const submit = e => {
-  e.preventDefault();
-  onNewColor(title, color);
-  setTitle("");
-  setColor("");
+const submit = event => {
+  event.preventDefault();
+  onNewColor(titleProps.value, colorProps.value);
+  resetTitle();
+  resetColor();
 };
 
   return (
-    <form onSubmit={submit}>
-        <input
-            value={title}
-            onChange={event => setTitle(event.target.value)}
-            type="text"
-            placeholder="color title..."
-            required
-        />
-        <input
-            value={color}
-            onChange={event => setColor(event.target.value)}
-            type="color"
-            required
-        />
-        <button>ADD</button>
-    </form>
-  )
+  <form onSubmit={submit}>
+    <input
+      {...titleProps}
+      type="text"
+      placeholder="color title..."
+      required
+    />
+    <input {...colorProps} type="color" required />
+    <button>ADD</button>
+  </form>
+);
 }
