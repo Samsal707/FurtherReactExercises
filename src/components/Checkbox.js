13ab6549f1c8cb0d32953b5f063b6da009616195
Samsal707 <samsal707@gmail.com>
2022-12-21 12:30:58 +1100

    This time the alert() is not reacheable - it will never execute after the component has rendered

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,9 +1,25 @@
 import React, { useState } from "react";
 
+// export default function Checkbox() {
+//     const [checked, setChecked] = useState(false);
+//     //here the alert is blocking - it appears before rendering and blocks rendering until the user dismissed it
+//     alert(`checked: ${checked.toString()}`);
+//   return (
+//     <>
+//       <input
+//         type="checkbox"
+//         value={checked}
+//         onChange={() => setChecked(checked => !checked)}
+//       />
+//       {checked ? "checked" : "not checked"}
+//     </>
+//   )
+// }
+
+
 export default function Checkbox() {
-    const [checked, setChecked] = useState(false);
-    //here the alert is blocking - it appears before rendering and blocks rendering until the user dismissed it
-    alert(`checked: ${checked.toString()}`);
+  const [checked, setChecked] = useState(false);
+
   return (
     <>
       <input
@@ -13,5 +29,8 @@ export default function Checkbox() {
       />
       {checked ? "checked" : "not checked"}
     </>
-  )
-}
+  );
+    // her the alert is placed at the end - it will never execute after rendering of the component
+  alert(`checked: ${checked.toString()}`);
+};
+
