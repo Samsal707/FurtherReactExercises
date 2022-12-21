c963bddb2e281e30aab3b177912ecaee21ff23ff
Samsal707 <samsal707@gmail.com>
2022-12-21 15:10:57 +1100

    Using useEffect

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React from "react";
-import ColorList from "./components/ColorList.js";
-import AddColorForm from "./components/AddColorForm";
-import Checkbox from "./components/Checkbox.js";
+// import React from "react";
+// import ColorList from "./components/ColorList.js";
+// import AddColorForm from "./components/AddColorForm";
+// import Checkbox from "./components/Checkbox.js";
 
-export default function App() {
-  return (
-    <>
-      <AddColorForm />
-      <ColorList />
-      <Checkbox />
-    </>
-  );
-}
+// export default function App() {
+//   return (
+//     <>
+//       <AddColorForm />
+//       <ColorList />
+//       <Checkbox />
+//     </>
+//   );
+// }
 
 // export default function App() {
 //   const [colors, setColors] = useState(colorData);
@@ -25,3 +25,43 @@ export default function App() {
 //     />
 //   );
 // }
+
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+export default function App() {
+  const [val, set] = useState("kevin");
+  
+  const [phrase, setPhrase] = useState("example phrase");
+
+  const createPhrase = () => {
+    setPhrase(val);
+    set("");
+  };
+
+  useEffect(() => {
+    console.log(`typing "${val}"`);
+  });
+
+  useEffect(() => {
+    console.log(`saved phrase: "${phrase}"`);
+    console.log(val)
+  });
+
+   useEffect(() => {
+    console.log(phrase);
+    console.log(val)
+  });
+
+  return (
+    <>
+      <label>Favorite phrase:</label>
+      <input
+        value={val}
+        placeholder={phrase}
+        onChange={e => set(e.target.value)}
+      />
+      <button onClick={createPhrase}>send</button>
+    </>
+  );
+}
